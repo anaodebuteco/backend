d2906c6e6a8ef546d3272dfcd1d19e6e6d646095
Pedro <anaodebuteco2@gmail.com>
2025-09-04 09:56:23 -0300

    Adicionando códigos da prática 3

---
+++ aulas/aula01/aula5/test/index.test.js
@@ -0,0 +1,5 @@
+test("se a >= b entao a - b >= 0",() => {
+  expect(calculadora.subtracao).toBeDefined();
+  expect(calculadora.subtracao).toBeDefined();
+  expect(calculadora.subtracao).toBeDefined();
+})
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import{soma, subtracao, multiplicacao, divisao }from "./index.js"
+
+console.log("teste da função soma()");
+if(soma(2, 2)==4)console.log("passou 1!");
+else console.log("falhou 2!");
